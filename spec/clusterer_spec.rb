707639b1bab27d78a5a22be02f0d2baf90cacfe9
Author Enfop <paulplatenius@gmail.com>
2019-02-05 20:19:55 +0100

    Add failing testsuite with identical points.

--- spec/clusterer_spec.rb
+++ spec/clusterer_spec.rb
@@ -1,4 +1,20 @@
 require 'spec_helper'
 describe Clusterer do
-  pending "write it"
+  context 'For identical points' do
+    before :context do
+      @input = [ 0, 1, 2 ],[ 0, 1, 2 ]
+    end
+
+    it 'It sets two clusters (unclustered, one for the identical points)' do
+      expect DBSCAN( @input, :epsilon => 4, :min_points => 1, :distance => :euclidean_distance ).clusters.size eq 2
+    end
+
+    it 'The Cluster for unclustered points is size 0' do
+      expect DBSCAN( @input, :epsilon => 4, :min_points => 1, :distance => :euclidean_distance ).clusters.size eq 0
+    end
+
+    it 'the Cluster 0 contains two points.' do
+      expect DBSCAN( @input, :epsilon => 4, :min_points => 1, :distance => :euclidean_distance ).clusters.size eq 2
+    end
+  end
 end
