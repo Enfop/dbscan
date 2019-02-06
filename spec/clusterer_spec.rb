6c60f61dac70f023500aa1cffbe7427898194082
Author Enfop <paulplatenius@gmail.com>
2019-02-06 22:37:10 +0100

    Fix specs

--- spec/clusterer_spec.rb
+++ spec/clusterer_spec.rb
@@ -6,15 +6,15 @@ describe Clusterer do
     end
 
     it 'It sets two clusters (unclustered, one for the identical points)' do
-      expect DBSCAN( @input, :epsilon => 4, :min_points => 1, :distance => :euclidean_distance ).clusters.size eq 2
+      expect(DBSCAN( @input, :epsilon => 4, :min_points => 1, :distance => :euclidean_distance ).clusters.size).to eq(2)
     end
 
     it 'The Cluster for unclustered points is size 0' do
-      expect DBSCAN( @input, :epsilon => 4, :min_points => 1, :distance => :euclidean_distance ).clusters.size eq 0
+      expect(DBSCAN( @input, :epsilon => 4, :min_points => 1, :distance => :euclidean_distance ).clusters[-1].size).to eq(0)
     end
 
     it 'the Cluster 0 contains two points.' do
-      expect DBSCAN( @input, :epsilon => 4, :min_points => 1, :distance => :euclidean_distance ).clusters.size eq 2
+      expect(DBSCAN( @input, :epsilon => 4, :min_points => 1, :distance => :euclidean_distance ).clusters.size).to eq(2)
     end
   end
 end
