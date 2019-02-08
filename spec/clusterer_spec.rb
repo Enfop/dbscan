1f923294a7b6d309870d44a0d1236eee61cdd776
Author Enfop <paulplatenius@gmail.com>
2019-02-08 22:03:55 +0100

    Add specs for two clusters with different points

--- spec/clusterer_spec.rb
+++ spec/clusterer_spec.rb
@@ -14,7 +14,29 @@ describe Clusterer do
     end
 
     it 'the Cluster 0 contains two points.' do
-      expect(DBSCAN( @input, :epsilon => 4, :min_points => 1, :distance => :euclidean_distance ).clusters.size).to eq(2)
+      expect(DBSCAN( @input, :epsilon => 4, :min_points => 1, :distance => :euclidean_distance ).clusters[0].size).to eq(2)
+    end
+  end
+
+  context 'For different points (2 clusters)' do
+    before :context do
+      @input = [ 0, 1, 2 ],[ 0, 1, 3 ],[ 10, 2, 3 ],[ 10, 2, 4 ]
+    end
+
+    it 'It sets three clusters (two clusters/one unclustered' do
+      expect(DBSCAN( @input, :epsilon => 4, :min_points => 1, :distance => :euclidean_distance ).clusters.size).to eq(3)
+    end
+
+    it 'The Cluster for unclustered points is size 0' do
+      expect(DBSCAN( @input, :epsilon => 4, :min_points => 1, :distance => :euclidean_distance ).clusters[-1].size).to eq(0)
+    end
+
+    it 'the Cluster 0 contains two points.' do
+      expect(DBSCAN( @input, :epsilon => 4, :min_points => 1, :distance => :euclidean_distance ).clusters[0].size).to eq(2)
+    end
+
+    it 'the Cluster 1 contains two points.' do
+      expect(DBSCAN( @input, :epsilon => 4, :min_points => 1, :distance => :euclidean_distance ).clusters[1].size).to eq(2)
     end
   end
 end
